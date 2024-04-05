fbf36e82f94a9aa643b546e85239bf689995e249
jabirmussa <jabirmussa185@gmail.com>
2024-04-06 01:23:00 +0200

    uu

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Montserrat } from "next/font/google";
-
-const montserrat = Montserrat({ subsets: ["latin"], weight: ['400', '500', '700'] });
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt">
       <Head>
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      <body className={montserrat.className}>
+      <body>
       </body>
     </Html>
   );
