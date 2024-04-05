7da496cf3cbd106a8227688ae53a36dc9d03562c
jabirmussa <jabirmussa185@gmail.com>
2024-04-06 00:56:57 +0200

    gooo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-const Home = () => {
+export default function Home() {
   return (
     <main className="overflow-hidden relative bg-purple-900 h-screen">
       <div className="wrapper max-w-7xl w-4/5 mx-auto">
         <div className="logo"><Image src={'/logo.png'} alt="logo" width={120} height={120} /></div>
         <p className="text-white text-lg">Agora podes fazer bixa <br /> em casa!</p>
         <button className="bg-yellow-400 rounded-md px-8 py-3">
-          <a href="main.html" className="flex items-center gap-2">
+          <a href={'/'} className="flex items-center gap-2">
             Começar!
             <Image src={'/Asset 3.svg'} alt="seta" width={24} height={24} />
           </a>
@@ -22,6 +22,4 @@ const Home = () => {
     </main>
   );
 };
-
-export default Home;
 
